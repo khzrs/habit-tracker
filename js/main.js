0a49d67059120ac0ebc5862a7b0f1df3d73a533b
Khizer Sohail <khizrsohail@gmail.com>
2026-02-21 07:05:59 +0000

    feat: add localStorage persistence

--- js/main.js
+++ js/main.js
@@ -1,1 +1,54 @@
-console.log("Habbit Tracker loaded")
+const form = document.getElementById("habit-form");
+const input = document.getElementById("habit-name");
+const list = document.getElementById("habit-list");
+
+let habits = [];
+
+const saved = localStorage.getItem("habits");
+if (saved) habits = JSON.parse(saved);
+
+function renderHabits(){
+    localStorage.setItem("habits", JSON.stringify(habits));
+
+    list.innerHTML = "";
+    //clears all existing habit items from the list so it can be redone without duplicates
+    //innerHTML is built-in DOM on HTML elements, gets or replaces all THML content inside an element
+
+    habits.forEach((habit, index) => { //array loop method taking the habit and index arguments
+        const li = document.createElement("li");
+
+        li.innerHTML = `
+        <label style="display:flex; align-items:center; gap:10px;">
+            <input type="checkbox" data-index="${index}" ${habit.completedToday ? "checked" : ""} />
+            <span> ${habit.name}</span>
+        </label>
+        `;
+
+        list.appendChild(li); 
+        //creates a newly created <li> element into the habit list on the page
+    
+    });
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const name = input.value.trim();
+    if(!name) return;
+
+    habits.push({ name, completedToday: false, streak: 0});
+
+    input.value = "";
+    renderHabits();
+});
+
+list.addEventListener("change", (e) => {
+    if (e.target.tagName != "INPUT" || e.target.type != "checkbox") return;
+
+    const index = Number(e.target.dataset.index);
+    habits[index].completedToday = e.target.checked;
+
+    renderHabits();
+});
+
+renderHabits();
